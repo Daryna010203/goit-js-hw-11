e017482941d501f7c079f2c3e16f847c28744835
Daryna Kurytsyna <152505147+Daryna010203@users.noreply.github.com>
2024-08-27 21:19:11 +0200

    update

--- src/main.js
+++ src/main.js
@@ -8,9 +8,32 @@ const galleryCards = document.querySelector('.js-gallery');
 
 const onSearchFormSubmit = event => {
     event.preventDefault();
+    const searchOfUsers = searchFormBtn.elements.user_query.value.trim();
+    if (searchOfUsers === '') {
+        alert('please enter your query into the search form')
+    } else {
+        fetch(`https://pixabay.com/api/?key=45521287-1fb3911845814b73b6d184262&q=${searchOfUsers}&image_type=photo&orientation=horizontal&safesearch=true`)
+        .then(response => {
+            if (!response.ok) {
+              throw new Error(response.status);
+            }
+      
+            return response.json();
+          })
+          .then(data => {
+            console.log(data);      
+            
+          })
+          .catch(err => {
+            console.log(err);
+          });
+      };
+      
+    }
+ 
 
-    console.log(searchFormBtn.elements.user_query.value);
-}
+    
+
 searchFormBtn.addEventListener('submit', onSearchFormSubmit);
 
 
